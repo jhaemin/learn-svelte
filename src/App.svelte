403e132b9b30216bdd094c8f01060b4f789e476b
Jang Haemin <io@jhaemin.com>
2021-02-06 12:35:00 +0900

    learn event and inline handler

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,14 @@
 <script lang="ts">
-async function getRandomNumber() {
-  const res = await fetch(`tutorial/random-number`)
-  const text = await res.text()
+let m = { x: 0, y: 0 }
 
-  if (res.ok) {
-    return text
-  } else {
-    throw new Error(text)
-  }
-}
-
-let promise = getRandomNumber()
-
-function handleClick() {
-  promise = getRandomNumber()
+function handleMousemove(event: MouseEvent) {
+  m.x = event.clientX
+  m.y = event.clientY
 }
 </script>
 
 <div class="app">
-  <button on:click={handleClick}>generate random number</button>
-
-  {#await promise}
-    <p>...waiting</p>
-  {:then number}
-    <p>The number is {number}</p>
-  {:catch error}
-    <p style="color: red">error</p>
-  {/await}
+  <div on:mousemove={(e) => (m = { x: e.clientX, y: e.clientY })}>
+    The mouse position is {m.x} x {m.y}
+  </div>
 </div>
