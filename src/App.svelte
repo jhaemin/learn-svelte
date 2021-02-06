d2a5e0050007a2333dcccdccfa03964ca6d3ac41
Jang Haemin <io@jhaemin.com>
2021-02-06 12:31:39 +0900

    learn promise

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
-let cats = [
-  { id: 'J---aiyznGQ', name: 'Keyboard Cat' },
-  { id: 'z_AbfPXTKms', name: 'Maru' },
-  { id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' },
-]
+async function getRandomNumber() {
+  const res = await fetch(`tutorial/random-number`)
+  const text = await res.text()
+
+  if (res.ok) {
+    return text
+  } else {
+    throw new Error(text)
+  }
+}
+
+let promise = getRandomNumber()
+
+function handleClick() {
+  promise = getRandomNumber()
+}
 </script>
 
 <div class="app">
-  {#each cats as cat, i (cat)}
-    <li>
-      <a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
-        {i + 1}: {cat.name}
-      </a>
-    </li>
-  {/each}
-</div>
+  <button on:click={handleClick}>generate random number</button>
 
-<style>
-</style>
+  {#await promise}
+    <p>...waiting</p>
+  {:then number}
+    <p>The number is {number}</p>
+  {:catch error}
+    <p style="color: red">error</p>
+  {/await}
+</div>
